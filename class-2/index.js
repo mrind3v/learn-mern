6cfee5f6323517c5947a30de7b4ccf8b29017145
mrind3v <mrinmaydev@outlook.comm>
2025-08-13 10:06:10 +0000

    added middleware to convert req body JSON to js object and added a POST route

--- class-2/index.js
+++ class-2/index.js
@@ -8,6 +8,9 @@ let courses = [
   { id: 3, name: "ML" },
 ];
 
+// middle to convert json data from req body to js object to be used in server side code
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
@@ -22,15 +25,15 @@ app.get("/about", (req, res) => {
 app.get("/courses/:id", (req, res) => {
   // id is part of req object
   console.log(req.params);
-  
+
   // method-1
-  //res.send(courses[req.params.id-1])
+  res.send(courses[req.params.id-1])
 
   // method-2
-  let course = courses.find((course) => {
-    course.id === parseInt(req.params.id);
-  });
-  res.send(course);
+// let course = courses.find((course) => {
+//     course.id === parseInt(req.params.id);
+//   });
+//   res.send(course);
 });
 
 // to get all courses
@@ -38,6 +41,19 @@ app.get("/courses", (req, res) => {
   res.send(courses); // res.send() changes js object to json automatically
 });
 
+// create a course (POST)
+app.post("/courses", (req,res)=>{
+    //let newCourse = {id: 4, name: "NodeJS"} // but newCourse should actually be sent by client
+    //courses.push(newCourse) 
+    let newCourse = {id: req.body.id, name: req.body.name}
+    // we get 500 internal error because req.body is json but newCourse is js object
+    // so we want every req to become js object using a middleware!
+    courses.push(req.body)
+    res.send(courses) // to see all courses 
+})
+
 app.listen(8001, () => {
   console.log("Server is running on PORT 8001");
 });
+
+// HW - find the difference between route and query params
