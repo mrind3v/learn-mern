1a8b5750682f9cf9c5861c8d9ac623c554bd3b47
mrind3v <mrinmaydev@outlook.comm>
2025-08-20 10:45:17 +0000

    created model to do CRUD operations

--- class-3-intro-to-mongodb/index.js
+++ class-3-intro-to-mongodb/index.js
@@ -7,6 +7,7 @@ const mongodbConnectionString =
   "mongodb+srv://mrind3v:max@cluster0.5r2z44c.mongodb.net/social?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(mongodbConnectionString)
@@ -42,6 +43,26 @@ let productSchema = new mongoose.Schema({
 // 2. next we will create collections in the database. We create Model to create collections
 // it means - "I want to create a 'products' collection that will follow 'productSchema'"
 let ProductModel = mongoose.model("products", productSchema);
+
+// 3. now actually creating a document (an actual product) in the products collection using POST req
+app.post("/api/products", async (req, res) => {
+  const body = req.body;
+  // to actually save the json body sent in POST req in the DB
+  // .create() is an async function as we're communicating with an external resource (DB here)
+  await ProductModel.create({
+    productName: body.productName,
+    productPrice: body.productPrice,
+    isInStock: body.isInStock,
+    category: body.category,
+    // can pass entire body also
+  });
+
+  res.status(201).json({ message: "product created successfully" });
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Server ON...");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
