38eaef3439b9ef54e546016329e19edf05a400c9
mrind3v <mrinmaydev@outlook.comm>
2025-08-20 10:24:00 +0000

    created mongodb schema and  model to create collection named Products

--- class-3-intro-to-mongodb/index.js
+++ class-3-intro-to-mongodb/index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = 8000;
-const mongodb_connection_string =
-"mongodb+srv://mrind3v:max@cluster0.5r2z44c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// we can specify name of our database after "/" and before "?" signs
+const mongodbConnectionString =
+  "mongodb+srv://mrind3v:max@cluster0.5r2z44c.mongodb.net/social?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
 
 mongoose
-  .connect(mongodb_connection_string)
+  .connect(mongodbConnectionString)
   .then(() => {
     console.log("DB Connected");
   })
@@ -16,7 +17,32 @@ mongoose
     console.error("Cannot Connect"); // mongoose.connect() is a promisified fn
   });
 
+// will create a schema
+let productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
 
-app.listen(8000, ()=>{
-    console.log(`Server running on PORT: ${PORT}`)
-})
+  productPrice: {
+    type: String,
+    required: true,
+  },
+
+  isInStock: {
+    type: Boolean,
+    required: true,
+  },
+
+  category: {
+    type: String,
+  },
+});
+
+// 2. next we will create collections in the database. We create Model to create collections
+// it means - "I want to create a 'products' collection that will follow 'productSchema'"
+let ProductModel = mongoose.model("products", productSchema);
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
